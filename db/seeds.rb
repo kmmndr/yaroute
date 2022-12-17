40c9526ec057ec8c79fcad23992e76d714e59c85
Thomas Kienlen <kommander@laposte.net>
2022-12-17 22:55:07 +0100

    🌱 Add first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  quiz = Quiz.where(title: 'Test').first_or_create
+
+  quiz.questions.create(title: 'question1').responses = [
+    Response.create(title: 'ResponseA', value: true),
+    Response.create(title: 'ResponseB', value: false),
+    Response.create(title: 'ResponseC', value: false),
+    Response.create(title: 'ResponseD', value: false)
+  ]
+
+  quiz.questions.create(title: 'question2').responses = [
+    Response.create(title: 'ResponseA', value: true),
+    Response.create(title: 'ResponseB', value: false),
+    Response.create(title: 'ResponseC', value: false),
+    Response.create(title: 'ResponseD', value: false)
+  ]
+
+  quiz.questions.create(title: 'question3', points: 5).responses = [
+    Response.create(title: 'ResponseA', value: true),
+    Response.create(title: 'ResponseB', value: false),
+    Response.create(title: 'ResponseC', value: false),
+    Response.create(title: 'ResponseD', value: false)
+  ]
+
+  game = Game.create(quiz: quiz, code: '123')
+
+  player1 = Player.create(game: game, name: 'player1')
+  player1.answers.create(
+    [
+      { response: quiz.questions[0].responses.first },
+      { response: quiz.questions[1].responses.first },
+      { response: quiz.questions[2].responses.last }
+    ]
+  )
+
+  player2 = Player.create(game: game, name: 'player2')
+  player2.answers.create(
+    [
+      { response: quiz.questions[0].responses.last },
+      { response: quiz.questions[1].responses.last },
+      { response: quiz.questions[2].responses.first }
+    ]
+  )
+end
