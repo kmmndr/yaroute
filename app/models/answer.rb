31a52fdbf3f94f5a327154d6bcba938699529692
Thomas Kienlen <kommander@laposte.net>
2022-12-17 23:06:01 +0100

    ✨ Add scopes for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,22 @@
 class Answer < ApplicationRecord
   belongs_to :player
   belongs_to :response
+
+  class << self
+    def good
+      joins(:response).where(response: { value: true })
+    end
+
+    def bad
+      joins(:response).where(response: { value: false })
+    end
+  end
+
+  def good?
+    response.value == true
+  end
+
+  def bad?
+    response.value == false
+  end
 end
