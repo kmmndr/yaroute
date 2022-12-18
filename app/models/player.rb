f7e7eb727c3c3c6361188b553fbb4aa5d9ea87b9
Thomas Kienlen <kommander@laposte.net>
2022-12-18 23:12:53 +0100

    ✨ Add score to players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,18 @@
 class Player < ApplicationRecord
   belongs_to :game
   has_many :answers
+
+  class << self
+    def sort_by_highest_score
+      joins(answers: { response: :question }).where(response: { value: true })
+                                             .group('players.id')
+                                             .select('players.*, SUM(questions.points) as score').order('score DESC')
+    end
+  end
+
+  def score
+    answers
+      .joins(:response).where(response: { value: true })
+      .joins(:question).sum(:points)
+  end
 end
