54f5884f634da12cb13187cd44f554afebaceb90
Thomas Kienlen <kommander@laposte.net>
2022-12-21 21:06:58 +0100

    💻 Allow remote access using bore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Allow remote access using bore/ngrok/...
+  config.hosts = nil
 end
