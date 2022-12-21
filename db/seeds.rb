30b213ee73cffb60040e3d525817aba8111d46d5
Thomas Kienlen <kommander@laposte.net>
2022-12-21 21:22:00 +0100

    🌱 Fix seed idempotency

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,44 +14,50 @@ if Rails.env.development?
 
   quiz = admin_user.quizzes.where(title: 'Test').first_or_create
 
-  quiz.questions.create(title: 'question1').responses = [
-    Response.create(title: 'ResponseA', value: true),
-    Response.create(title: 'ResponseB', value: false),
-    Response.create(title: 'ResponseC', value: false),
-    Response.create(title: 'ResponseD', value: false)
-  ]
-
-  quiz.questions.create(title: 'question2').responses = [
-    Response.create(title: 'ResponseA', value: true),
-    Response.create(title: 'ResponseB', value: false),
-    Response.create(title: 'ResponseC', value: false),
-    Response.create(title: 'ResponseD', value: false)
-  ]
-
-  quiz.questions.create(title: 'question3', points: 5).responses = [
-    Response.create(title: 'ResponseA', value: true),
-    Response.create(title: 'ResponseB', value: false),
-    Response.create(title: 'ResponseC', value: false),
-    Response.create(title: 'ResponseD', value: false)
-  ]
-
-  game = Game.create(quiz: quiz, code: '123')
-
-  player1 = Player.create(game: game, name: 'player1')
-  player1.answers.create(
-    [
-      { response: quiz.questions[0].responses.first },
-      { response: quiz.questions[1].responses.first },
-      { response: quiz.questions[2].responses.last }
+  if quiz.questions.none?
+    quiz.questions.create(title: 'question1').responses = [
+      Response.create(title: 'ResponseA', value: true),
+      Response.create(title: 'ResponseB', value: false),
+      Response.create(title: 'ResponseC', value: false),
+      Response.create(title: 'ResponseD', value: false)
     ]
-  )
 
-  player2 = Player.create(game: game, name: 'player2')
-  player2.answers.create(
-    [
-      { response: quiz.questions[0].responses.last },
-      { response: quiz.questions[1].responses.last },
-      { response: quiz.questions[2].responses.first }
+    quiz.questions.create(title: 'question2').responses = [
+      Response.create(title: 'ResponseA', value: true),
+      Response.create(title: 'ResponseB', value: false),
+      Response.create(title: 'ResponseC', value: false),
+      Response.create(title: 'ResponseD', value: false)
     ]
-  )
+
+    quiz.questions.create(title: 'question3', points: 5).responses = [
+      Response.create(title: 'ResponseA', value: true),
+      Response.create(title: 'ResponseB', value: false),
+      Response.create(title: 'ResponseC', value: false),
+      Response.create(title: 'ResponseD', value: false)
+    ]
+  end
+
+  if quiz.games.none?
+    game = quiz.games.create(code: '123')
+
+    player1 = game.players.create(name: 'player1')
+    player1.answers.create(
+      [
+        { response: quiz.questions[0].responses.first },
+        { response: quiz.questions[1].responses.first },
+        { response: quiz.questions[2].responses.last }
+      ]
+    )
+    player1.user.create_account(login: 'player1', password: 'player1')
+
+    player2 = game.players.create(name: 'player2')
+    player2.answers.create(
+      [
+        { response: quiz.questions[0].responses.last },
+        { response: quiz.questions[1].responses.last },
+        { response: quiz.questions[2].responses.first }
+      ]
+    )
+    player2.user.create_account(login: 'player2', password: 'player2')
+  end
 end
