34d6e780e8b6942e2e774117d479f6d098f5ff6a
Thomas Kienlen <kommander@laposte.net>
2023-01-20 19:15:08 +0100

    ✨ Add authorization base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   include Authenticable
+  include Authorizable
   include AutoRefreshable
 
   before_action :authenticate
+
+  rescue_from Yaroute::Exceptions::ForbiddenError, with: ->(_exception) { forbid }
 end
