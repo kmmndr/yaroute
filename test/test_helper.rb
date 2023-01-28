493cf8c2a8222af1f0ad95791fa34c305c69573b
Thomas Kienlen <kommander@laposte.net>
2023-01-28 15:57:59 +0100

    🧵 Avoid tests parallelization

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,10 +18,7 @@ end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
 end
