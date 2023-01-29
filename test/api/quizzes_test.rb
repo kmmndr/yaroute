57227d97ff1075718385751a306a9368d8c4e476
Thomas Kienlen <kommander@laposte.net>
2023-01-29 21:54:11 +0100

    🛂 Test quizzes API permissions

--- test/api/quizzes_test.rb
+++ test/api/quizzes_test.rb
@@ -11,11 +11,15 @@ class Yaroute::API::QuizzesTest < MiniTest::Test
 
   def setup
     @user1 = Account.create(login: 'user1', password: 'secret').user
-
-    add_authorization_header(login: 'user1', password: 'secret')
+    @user2 = Account.create(login: 'user2', password: 'secret').user
   end
 
   def test_get_api_quizzes_returns_an_empty_array_of_quizzes
+    get '/api/v1/quizzes.json'
+    assert_equal 401, last_response.status
+
+    add_authorization_header(login: 'user1', password: 'secret')
+
     get '/api/v1/quizzes.json'
     assert last_response.ok?
     assert_equal [], JSON.parse(last_response.body)
@@ -29,21 +33,36 @@ class Yaroute::API::QuizzesTest < MiniTest::Test
     parsed_quiz = parsed_quizzes.first
     assert_equal 'foo', parsed_quiz['title']
     assert_nil parsed_quiz['questions']
+
+    add_authorization_header(login: 'user2', password: 'secret')
+
+    get '/api/v1/quizzes.json'
+    assert last_response.ok?
+    assert_equal [], JSON.parse(last_response.body)
   end
 
   def test_get_api_quizzes_id_returns_a_quiz_by_id
     quiz = FactoryBot.create(:quiz, questions_count: 3, title: 'foo', user: @user1)
     assert_equal 3, quiz.questions.count
 
+    add_authorization_header(login: 'user1', password: 'secret')
+
     get "/api/v1/quizzes/#{quiz.id}.json"
     parsed_quiz = JSON.parse(last_response.body)
     assert_equal quiz.title, parsed_quiz['title']
     assert_equal 3, parsed_quiz['questions'].count
+
+    add_authorization_header(login: 'user2', password: 'secret')
+
+    get "/api/v1/quizzes/#{quiz.id}.json"
+    assert_equal 403, last_response.status
   end
 
   def test_get_api_quiz_games_returns_games
     quiz = FactoryBot.create(:quiz, games_count: 2, title: 'foo', user: @user1)
     quiz.games.create(user: quiz.user, code: '1234')
+
+    add_authorization_header(login: 'user1', password: 'secret')
 
     get "/api/v1/quizzes/#{quiz.id}/games.json"
     parsed_games = JSON.parse(last_response.body)
@@ -51,5 +70,10 @@ class Yaroute::API::QuizzesTest < MiniTest::Test
 
     parsed_last_game = parsed_games.last
     assert_equal '1234', parsed_last_game['code']
+
+    add_authorization_header(login: 'user2', password: 'secret')
+
+    get "/api/v1/quizzes/#{quiz.id}/games.json"
+    assert_equal 403, last_response.status
   end
 end
